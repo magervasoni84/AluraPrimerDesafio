862d05eb4d82a538578474edf34949f29321344f
mariano <m.a.gervasoni84@gmail.com>
2022-08-18 19:43:16 -0300

    Inicio con Js

---
+++ encriptarDesencritar.js
@@ -0,0 +1,8 @@
+var botonEncriptar = document.querySelector(".btn-encriptar"); // Recupero el elemento
+var botonDesencriptar = document.querySelector(".btn-desencriptars"); 
+
+botonEncriptar.onclick = encriptar; //Sin () para que no la llame al recargarse la pagina
+
+function encriptar(){
+    
+}
